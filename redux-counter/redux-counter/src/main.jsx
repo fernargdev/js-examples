a7abf159ece78e0ce43f015bfb30a07ae7aaea93
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-03-30 13:15:30 -0400

    crear redux-count basico

--- redux-counter/redux-counter/src/main.jsx
+++ redux-counter/redux-counter/src/main.jsx
@@ -1,5 +1,47 @@
+/* eslint-disable no-unused-vars */
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import { createStore } from 'redux'
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+// eslint-disable-next-line react-refresh/only-export-components
+function App() {
+  return (
+    <>
+      <div className="card">
+        <div>{store.getState()}</div>
+        <button onClick={(e) => store.dispatch({ type: 'INCREMENT' })}>
+          +
+        </button>
+        <button onClick={(e) => store.dispatch({ type: 'DECREMENT' })}>
+          -
+        </button>
+        <button onClick={(e) => store.dispatch({ type: 'ZERO' })}>0</button>
+      </div>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
