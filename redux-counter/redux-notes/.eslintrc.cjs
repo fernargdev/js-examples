c714c5866724a3b18bc7f54e2fd02a949fc83e9e
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-03-30 18:10:20 -0400

    realizando configuraciones

--- redux-counter/redux-notes/.eslintrc.cjs
+++ redux-counter/redux-notes/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, 'jest/globals': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
