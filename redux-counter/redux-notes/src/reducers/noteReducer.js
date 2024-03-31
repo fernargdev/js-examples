41aaad0c45f4261d9cfa8b21543c625c7e6e31a9
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-03-30 23:48:38 -0400

    usando la sintaxis de extension de matriz

--- redux-counter/redux-notes/src/reducers/noteReducer.js
+++ redux-counter/redux-notes/src/reducers/noteReducer.js
@@ -1,7 +1,7 @@
 const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
-      return state.concat(action.payload)
+      return [...state, action.payload]
 
     case 'TOGGLE_IMPORTANCE': {
       const id = action.payload
