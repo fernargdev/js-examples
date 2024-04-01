4b57cf428bf6598cd987528979b25d21bf44b649
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-01 19:30:56 -0400

    fix bug in 'TOGGLE_IMPORTANCE'

--- redux-counter/redux-notes/src/reducers/noteReducer.js
+++ redux-counter/redux-notes/src/reducers/noteReducer.js
@@ -4,12 +4,16 @@ const noteReducer = (state = [], action) => {
       return [...state, action.payload]
 
     case 'TOGGLE_IMPORTANCE': {
-      const id = action.payload
+      const id = action.payload.id
       const noteToChange = state.find((n) => n.id === id)
+
+      if (!noteToChange) return state
+
       const changedNote = {
         ...noteToChange,
         important: !noteToChange.important,
       }
+
       return state.map((n) => (n.id !== id ? n : changedNote))
     }
 
