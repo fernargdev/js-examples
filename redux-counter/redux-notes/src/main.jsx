554e37dcccea0526d3b5441a107043566ac66478
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-01 19:55:43 -0400

    funcionalidad para agregar nuevas notas y cambiar su importancia

--- redux-counter/redux-notes/src/main.jsx
+++ redux-counter/redux-notes/src/main.jsx
@@ -25,13 +25,41 @@ store.dispatch({
   },
 })
 
+const generateId = () => Number((Math.random() * 10000000).toFixed(0))
+
 // eslint-disable-next-line react-refresh/only-export-components
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id },
+    })
+  }
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+
       <ul>
         {store.getState().map((note) => (
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         ))}
