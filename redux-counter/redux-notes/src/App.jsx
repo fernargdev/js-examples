e9ac6c217b538467f975e0f6549fcbf9814fb4cb
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-02 12:16:35 -0400

    separar la lista de notas

--- redux-counter/redux-notes/src/App.jsx
+++ redux-counter/redux-notes/src/App.jsx
@@ -1,26 +1,12 @@
 import './App.css'
 import NewNote from './components/NewNote'
-
-import { toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector } from 'react-redux'
+import Notes from './components/Notes'
 
 const App = () => {
-  const notes = useSelector((state) => state)
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id))
-  }
-
   return (
     <div>
       <NewNote />
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        ))}
-      </ul>
+      <Notes />
     </div>
   )
 }
