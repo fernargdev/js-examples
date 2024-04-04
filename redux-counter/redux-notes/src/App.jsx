37e19db75031bd86a2b538fcfcacdc2198f0cf2a
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-04 14:33:30 -0400

    crear grupo de botones

--- redux-counter/redux-notes/src/App.jsx
+++ redux-counter/redux-notes/src/App.jsx
@@ -3,9 +3,35 @@ import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value)
+  }
+
   return (
     <div>
       <NewNote />
+
+      <div>
+        all{' '}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('ALL')}
+        />
+        important{' '}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('IMPORTANT')}
+        />
+        nonimportant{' '}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')}
+        />
+      </div>
+
       <Notes />
     </div>
   )
