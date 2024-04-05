ef174e015ba308ff4d1c08dda1fa77bab678f1cd
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-04 20:54:04 -0400

    usando VisibilityFilter en Notes

--- redux-counter/redux-notes/src/App.jsx
+++ redux-counter/redux-notes/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
