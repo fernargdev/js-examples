995ed51f0cbf98225a0e2721ac3ca10c8bda7e5a
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-04 20:35:45 -0400

    creando filter y combinando reducer.

--- redux-counter/redux-notes/src/main.jsx
+++ redux-counter/redux-notes/src/main.jsx
@@ -1,13 +1,28 @@
 import './index.css'
 import ReactDOM from 'react-dom/client'
 
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+})
+
+const store = createStore(reducer)
+console.log(store.getState())
+
+// Simulation
+// import { createNote } from './reducers/noteReducer'
+// import { filterChange } from './reducers/filterReducer'
+
+// store.subscribe(() => console.log(store.getState()))
+// store.dispatch(filterChange('IMPORTANT'))
+// store.dispatch(createNote('New note'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
