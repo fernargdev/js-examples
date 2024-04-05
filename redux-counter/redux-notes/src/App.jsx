4434844e16f03fb277abe67ef9a660b0762602bb
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-04 20:49:40 -0400

    create VisibilityFilter

--- redux-counter/redux-notes/src/App.jsx
+++ redux-counter/redux-notes/src/App.jsx
@@ -3,35 +3,9 @@ import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value)
-  }
-
   return (
     <div>
       <NewNote />
-
-      <div>
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('ALL')}
-        />
-        all{' '}
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('IMPORTANT')}
-        />
-        important{' '}
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('NONIMPORTANT')}
-        />
-        nonimportant{' '}
-      </div>
-
       <Notes />
     </div>
   )
