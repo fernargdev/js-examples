23af28a28859520c90cba76e66eaef06d3200a4d
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-06 19:49:29 -0400

    cambiando la creacion de la tienda en main y el reductor y los creadores en noteReducer

--- redux-counter/redux-notes/src/main.jsx
+++ redux-counter/redux-notes/src/main.jsx
@@ -1,19 +1,21 @@
 import './index.css'
 import ReactDOM from 'react-dom/client'
 
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 })
 
-const store = createStore(reducer)
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- redux-counter/redux-notes/src/reducers/noteReducer.js
+++ redux-counter/redux-notes/src/reducers/noteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 const initialState = [
   {
     content: 'reducer defines how redux store works',
@@ -11,48 +13,68 @@ const initialState = [
   },
 ]
 
-const noteReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'NEW_NOTE':
-      return [...state, action.payload]
+const generateId = () => Number((Math.random() * 10000000).toFixed(0))
 
-    case 'TOGGLE_IMPORTANCE': {
-      const id = action.payload.id
+const noteSlice = createSlice({
+  name: 'notes',
+  initialState,
+  reducers: {
+    createNote(state, action) {
+      const content = action.payload
+      state.push({ content, important: false, id: generateId() })
+    },
+    toggleImportanceOf(state, action) {
+      const id = action.payload
       const noteToChange = state.find((n) => n.id === id)
-
-      if (!noteToChange) return state
-
       const changedNote = {
         ...noteToChange,
         important: !noteToChange.important,
       }
-
-      return state.map((n) => (n.id !== id ? n : changedNote))
-    }
-
-    default:
-      return state
-  }
-}
-
-const generateId = () => Number((Math.random() * 10000000).toFixed(0))
-
-export const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    payload: {
-      content,
-      important: false,
-      id: generateId(),
+      return state.map((note) => (note.id !== id ? note : changedNote))
     },
-  }
-}
+  },
+})
 
-export const toggleImportanceOf = (id) => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    payload: { id },
-  }
-}
+// const noteReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'NEW_NOTE':
+//       return [...state, action.payload]
+
+//     case 'TOGGLE_IMPORTANCE': {
+//       const id = action.payload.id
+//       const noteToChange = state.find((n) => n.id === id)
+
+//       if (!noteToChange) return state
+
+//       const changedNote = {
+//         ...noteToChange,
+//         important: !noteToChange.important,
+//       }
+
+//       return state.map((n) => (n.id !== id ? n : changedNote))
+//     }
+
+//     default:
+//       return state
+//   }
+// }
+
+// export const createNote = (content) => {
+//   return {
+//     type: 'NEW_NOTE',
+//     payload: {
+//       content,
+//       important: false,
+//       id: generateId(),
+//     },
+//   }
+// }
+
+// export const toggleImportanceOf = (id) => {
+//   return {
+//     type: 'TOGGLE_IMPORTANCE',
+//     payload: { id },
+//   }
+// }
 
 export default noteReducer
