d1e0f51dae3b2e73b18f52ef26cbee61d6209025
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-06 21:56:42 -0400

    usando current para el log con redux

--- redux-counter/redux-notes/src/reducers/noteReducer.js
+++ redux-counter/redux-notes/src/reducers/noteReducer.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, current } from '@reduxjs/toolkit'
 
 const initialState = [
   {
@@ -21,15 +21,21 @@ const noteSlice = createSlice({
   reducers: {
     createNote(state, action) {
       const content = action.payload
+
       state.push({ content, important: false, id: generateId() })
     },
+
     toggleImportanceOf(state, action) {
       const id = action.payload
       const noteToChange = state.find((n) => n.id === id)
+
       const changedNote = {
         ...noteToChange,
         important: !noteToChange.important,
       }
+
+      console.log(current(state))
+
       return state.map((note) => (note.id !== id ? note : changedNote))
     },
   },
