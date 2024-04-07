e34f8ce999bb6d5191bf9a7d4f990015e60df1cf
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-06 21:39:35 -0400

    modificando los nombres de los tipos de acción en las pruebas

--- redux-counter/redux-notes/src/reducers/noteReducer.test.js
+++ redux-counter/redux-notes/src/reducers/noteReducer.test.js
@@ -5,12 +5,8 @@ describe('noteReducer', () => {
   test('returns new state with action NEW_NOTE', () => {
     const state = []
     const action = {
-      type: 'NEW_NOTE',
-      payload: {
-        content: 'the app state is in redux store',
-        important: true,
-        id: 1,
-      },
+      type: 'notes/createNote',
+      payload: 'the app state is in redux store',
     }
 
     deepFreeze(state)
@@ -35,10 +31,8 @@ describe('noteReducer', () => {
     ]
 
     const action = {
-      type: 'TOGGLE_IMPORTANCE',
-      payload: {
-        id: 2,
-      },
+      type: 'notes/toggleImportanceOf',
+      payload: 2,
     }
 
     deepFreeze(state)
