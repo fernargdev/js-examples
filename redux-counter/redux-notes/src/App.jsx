43e9d212fbcdb6d385732246f7749fab69cd1822
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-12 21:30:30 -0400

    cambiando la inicializacion al componente App

--- redux-counter/redux-notes/src/App.jsx
+++ redux-counter/redux-notes/src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
+
+import { useEffect } from 'react'
+
+import noteService from './sevices/notes'
+import { setNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    noteService.getAll().then((notes) => dispatch(setNotes(notes)))
+  }, [])
+
   return (
     <div>
       <NewNote />
--- redux-counter/redux-notes/src/main.jsx
+++ redux-counter/redux-notes/src/main.jsx
@@ -4,9 +4,6 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 
-import noteService from './sevices/notes'
-import noteReducer, { setNotes } from './reducers/noteReducer'
-
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
@@ -17,10 +14,6 @@ const store = configureStore({
     filter: filterReducer,
   },
 })
-
-noteService.getAll().then((notes) => store.dispatch(setNotes(notes)))
-
-console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
