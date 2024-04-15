9856854efc736542cbb7d82da8269ce5a30f5cac
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-15 17:49:43 -0400

    instalar y adecciones react-query a la app

--- redux-counter/query-notes/src/main.jsx
+++ redux-counter/query-notes/src/main.jsx
@@ -1,4 +1,12 @@
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+)
