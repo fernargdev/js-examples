b9d7942e7411fd930a4bc29d0d146b550291f8db
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-20 17:51:57 -0400

    usando el useReducer

--- redux-counter/hook-counter/src/App.jsx
+++ redux-counter/hook-counter/src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
 
-function App() {
+import { useReducer } from 'react'
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1
+    case 'DEC':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const App = () => {
+  const [counter, counterDispatch] = useReducer(counterReducer, 0)
+
   return (
-    <>
-      <h1>Hola Mundo</h1>
-    </>
+    <div>
+      <div>{counter}</div>
+      <div>
+        <button onClick={() => counterDispatch({ type: 'INC' })}>+</button>
+        <button onClick={() => counterDispatch({ type: 'DEC' })}>-</button>
+        <button onClick={() => counterDispatch({ type: 'ZERO' })}>0</button>
+      </div>
+    </div>
   )
 }
 
