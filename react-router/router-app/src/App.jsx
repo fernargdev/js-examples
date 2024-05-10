4ffea2bd8ba77a2d3174a797bede272ccedae85a
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-05-10 19:49:05 -0400

    empezando a usar react-router

--- react-router/router-app/src/App.jsx
+++ react-router/router-app/src/App.jsx
@@ -1,67 +1,66 @@
 import './App.css'
 import './index.css'
 
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 const Home = () => (
   <div>
-    {' '}
-    <h2>TKTL notes app</h2>{' '}
+    <h2>TKTL notes app</h2>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </p>
   </div>
 )
 
 const Notes = () => (
   <div>
-    {' '}
-    <h2>Notes</h2>{' '}
+    <h2>Notes</h2>
   </div>
 )
 
 const Users = () => (
   <div>
-    {' '}
-    <h2>Users</h2>{' '}
+    <h2>Users</h2>
   </div>
 )
 
 const App = () => {
-  const [page, setPage] = useState('home')
-
-  const toPage = (page) => (event) => {
-    event.preventDefault()
-    setPage(page)
-  }
-
-  const content = () => {
-    if (page === 'home') {
-      return <Home />
-    } else if (page === 'notes') {
-      return <Notes />
-    } else if (page === 'users') {
-      return <Users />
-    }
-  }
-
   const padding = {
     padding: 5,
   }
 
   return (
-    <div>
+    <Router>
       <div>
-        <a href="" onClick={toPage('home')} style={padding}>
+        <Link style={padding} to="/">
           home
-        </a>
-        <a href="" onClick={toPage('notes')} style={padding}>
+        </Link>
+        <Link style={padding} to="/notes">
           notes
-        </a>
-        <a href="" onClick={toPage('users')} style={padding}>
+        </Link>
+        <Link style={padding} to="/users">
           users
-        </a>
+        </Link>
       </div>
 
-      {content()}
-    </div>
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Note app, Department of Computer Science 2024</i>
+      </div>
+    </Router>
   )
 }
 
