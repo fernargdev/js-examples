d526692df6f9ca8786e079bf7b2773af775ee37b
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-05-19 13:08:40 -0400

    implementar counter app

--- custom-hooks/src/App.jsx
+++ custom-hooks/src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css'
 
+import { useState } from 'react'
+
 const App = () => {
-  return <div>App</div>
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+      <button onClick={() => setCounter(counter - 1)}>minus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
+    </div>
+  )
 }
 
 export default App
