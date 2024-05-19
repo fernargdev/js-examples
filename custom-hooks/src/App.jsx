18c9c28475a3e06c094689bb232ad6df68d91e34
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-05-19 13:41:43 -0400

    create click app

--- custom-hooks/src/App.jsx
+++ custom-hooks/src/App.jsx
@@ -27,13 +27,28 @@ const useCounter = () => {
 
 const App = () => {
   const counter = useCounter()
+  const left = useCounter()
+  const right = useCounter()
 
   return (
     <div>
-      <div>{counter.value}</div>
-      <button onClick={counter.increase}>plus</button>
-      <button onClick={counter.decrease}>minus</button>
-      <button onClick={counter.zero}>zero</button>
+      <div>
+        <h2>Counter App</h2>
+        <div>{counter.value}</div>
+        <button onClick={counter.increase}>plus</button>
+        <button onClick={counter.decrease}>minus</button>
+        <button onClick={counter.zero}>zero</button>
+      </div>
+
+      <div>
+        <h2>Click App</h2>
+
+        <div>{left.value}</div>
+        <button onClick={left.increase}>left</button>
+
+        <div>{right.value}</div>
+        <button onClick={right.increase}>right</button>
+      </div>
     </div>
   )
 }
