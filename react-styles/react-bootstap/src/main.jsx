3a44ccee48c31b70a93e826f172c4e1b5f24ab4b
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-05-20 18:41:52 -0400

    usando boostrap Table

--- react-styles/react-bootstap/src/main.jsx
+++ react-styles/react-bootstap/src/main.jsx
@@ -17,6 +17,8 @@ import {
   useMatch,
 } from 'react-router-dom'
 
+import { Table } from 'react-bootstrap'
+
 const Home = () => (
   <div>
     <h2>TKTL notes app</h2>
@@ -49,13 +51,18 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map((note) => (
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      ))}
-    </ul>
+    <Table striped>
+      <tbody>
+        {notes.map((note) => (
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>{note.content}</Link>
+            </td>
+            <td>{note.user}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   </div>
 )
 
