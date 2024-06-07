8a6ac377a9ee4be0460c37d65e4fd8f8b1998ecd
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-06-06 21:08:21 -0400

    usand useNotes

--- webpack/src/App.js
+++ webpack/src/App.js
@@ -1,18 +1,35 @@
-import React, { useState } from 'react'
-import './index.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [values, setValues] = useState([])
 
+  const url = 'https://notes2023.fly.dev/api/notes'
+  const notes = useNotes(url)
+
   const handleClick = () => {
     setCounter(counter + 1)
     setValues(values.concat(counter))
   }
+
   return (
     <div className="container">
       hello webpack {counter} clicks
       <button onClick={handleClick}>press</button>
+      <div>
+        {notes.length} notes on server {url}
+      </div>
     </div>
   )
 }
