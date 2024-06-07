d68e6660659bd84a176332766b601f5fcfed570c
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-06-07 01:08:01 -0400

    usando prolifill

--- webpack/src/App.js
+++ webpack/src/App.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import PromisePolyfill from 'promise-polyfill'
+
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
+
 const useNotes = (url) => {
   const [notes, setNotes] = useState([])
   useEffect(() => {
