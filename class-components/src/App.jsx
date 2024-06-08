eccfda2cf0be18888f623651ef8182990eebf170
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-06-08 15:20:08 -0400

    creando version inicial de un componente de clase

--- class-components/src/App.jsx
+++ class-components/src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css'
 
-const App = () => {
-  return <div>Hello</div>
+import React from 'react'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!!!</h1>
+      </div>
+    )
+  }
 }
 
 export default App
